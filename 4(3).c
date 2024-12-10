30121013b06cb59359cc8b4145bb995a1f386eb7
kumaran3127 <kumaranklslv@gmail.com>
2024-12-10 15:20:51 +0530

    Add files via upload

---
+++ 4(3).c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+# include <stdbool.c>
+void searchelement(int matrix[100][100],int rows,int cols,int target)
+{
+    int i=0,=cols-1;
+    while(i<=rows && j>=0)
+    {
+        if(matrix[i][j]==target)
+        {
+            printf("element %D found at position (%d,%d)/n",target,i,j);
+            return ;
+        }
+
+    }
+}
